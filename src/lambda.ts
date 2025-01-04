78ad32b327962e2c093d442ba33f2c948a61e862
boms03 <dvboms@gmail.com>
2025-01-04 13:35:39 +0900

    feat: add automated login to hywep
    Feat: Add automated login function for HYWEP
    
    Automates the login process on the HYWEP platform using Puppeteer.
    
    - Navigates to the HYWEP homepage and triggers the login modal.
    - Accepts dialog prompts during the login process.
    - Fills in credentials (`HYWE_LOGIN_USERNAME` and `HYWE_LOGIN_PASSWORD`) from environment variables.
    - Returns cookies in a simplified format containing only `name` and `value`.
    
    This streamlines authentication for automated workflows and ensures secure handling of user credentials.

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
 import * as puppeteer from 'puppeteer-core';
+import {Page} from "puppeteer-core/lib/cjs/puppeteer/common/Page";
 
 const chromium = require('chrome-aws-lambda');
 
@@ -22,4 +23,27 @@ async function launchBrowser() {
         ? chromium.defaultViewport
         : { width: 1280, height: 800 },
   });
+}
+
+async function login(page: Page) {
+  console.log('Starting login process...');
+
+  page.on('dialog', async (dialog) => {
+    await dialog.accept();
+  });
+
+  await page.goto('https://hywep.hanyang.ac.kr/index.do', {
+    timeout: 30000,
+    waitUntil: 'domcontentloaded',
+  });
+
+  await page.click('a[onclick="return topLogin(\'1\')"] .log_btn');
+
+  await page.waitForSelector('#uid');
+  await page.type('#uid', process.env.HYWE_LOGIN_USERNAME!);
+  await page.type('#upw', process.env.HYWE_LOGIN_PASSWORD!);
+  await page.click('#login_btn');
+
+  const cookies = await page.cookies();
+  return cookies.map(({name, value}) => ({name, value}));
 }
